f1242069aa703b4e771b04813b1a140ed62c0f9c
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-04-16 11:46:37 -0400

    Add definitions for declatrations using . trick

---
+++ vim_tut/ex1.py
@@ -0,0 +1,14 @@
+"""
+Convert C function declarations to function definitions, using as few key strokes as possible
+Steps :
+1. Navigate to the first line using h,j,k,l
+2. 'A<backspace>{<ret>}<ret><esc>'
+3. Goto other line and hit . to repeat. The functions will be expanded
+
+
+"""
+
+int RetScore(int id);
+bool IsEmpty(Object obj, int id);
+char retCode(int *arr, string s);
+
