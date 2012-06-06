feba943c153c8310a64469510693d3bf32d91c85
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2012-06-06 16:47:13 -0400

    Added code for plotting across monomers

--- junk/draw_hexamer_3H4E.py
+++ junk/draw_hexamer_3H4E.py
@@ -10,6 +10,7 @@ pymol.finish_launching()
 from pymol import cmd
 
 import glob;
+import itertools 
 from itertools import combinations
 
 
@@ -41,15 +42,6 @@ cmd.util.cbc()
 #cmd.show('spheres','retinal')
 #cmd.color('magenta','retinal')
 
-# Set the view for monomer
-#cmd.set_view(\
-#    '-0.912057340,   -0.295883119,   -0.283901036,\
-#     0.399889141,   -0.795006156,   -0.456119299,\
-#    -0.090745762,   -0.529538393,    0.843419492,\
-#     0.000087761,   -0.000581503, -183.590560913,\
-#     8.321296692,  -66.967765808,  -33.590000153,\
-#   -36.799331665,  403.937011719,  -20.000000000' )
-
 # Set the view from top
 cmd.set_view (\
      '0.455506235,   -0.389297336,    0.800600052,\
@@ -59,14 +51,6 @@ cmd.set_view (\
    -37.810100555,  -63.827388763,  -22.882722855,\
    221.616317749,  411.317382812,  -20.000000000' )
 
-# Set the view from the bottom
-#cmd.set_view (\
-#     0.509821594,    0.429360747,   -0.745473742,\
-#    -0.444633365,   -0.610314071,   -0.655602694,\
-#    -0.736464798,    0.665700138,   -0.120242476,\
-#     0.000000000,    0.000000000, -294.413665771,\
-#   -13.488616943,  -48.363307953,  -22.056068420,\
-#   232.117980957,  356.709350586,  -20.000000000 )
 
 
 # constants
@@ -78,30 +62,90 @@ with open(edgepath,'r') as fin :
 edges = [line.strip().split() for line in data]
 
 chains = ['A','B','C','D','E','F']
-comb_chains = combinations(chains,2)
+
+
 
 for n,e in  enumerate(edges) : 
     for ch in chains : 
-       t = cmd.dist('dist'+str(n),'3H4E//'+ch+'/'+e[0]+'/CA',\
+       t = cmd.dist('dist'+ch+str(n),'3H4E//'+ch+'/'+e[0]+'/CA',\
 		'3H4E//'+ch+'/'+e[1]+'/CA')
 
        if t < cutoff  : 
-           cmd.delete('dist'+str(n)) 
+           cmd.delete('dist'+ch+str(n)) 
        else : 
-           cmd.color('blue','dist'+str(n))
-           cmd.hide('labels','dist'+str(n))
+           cmd.color('blue','dist'+ch+str(n))
+           cmd.hide('labels','dist'+ch+str(n))
            cmd.show('spheres','3H4E//'+ch+'/'+e[0]+'/CA')
            cmd.show('spheres','3H4E//'+ch+'/'+e[1]+'/CA')
            cmd.color('yellow','3H4E//'+ch+'/'+e[0]+'/CA')
            cmd.color('yellow','3H4E//'+ch+'/'+e[1]+'/CA')
+
+# Create a set of residues
+set_res = []
+for e_tup in edges : 
+	set_res.extend(e_tup)
+set_res = set(set_res)
+
+for ch,resi in itertools.product(chains,set_res) : 
+	cmd.label('3H4E//'+ch+'/'+resi+'/CA','"%s%s"%(resn,resi)')
 
 cmd.set('dash_radius','0.2');
 cmd.set('dash_gap','0.0');
 cmd.set('sphere_scale','0.3')
 #cmd.set('sphere_scale','0.8','retinal');
 #cmd.ray()
+outfp = os.path.join(DATA_DIR,os.path.splitext(edgef)[0]+'_hex_top.png')
 print('*'*5+'Image:%s'%os.path.abspath(outfp)+'*'*5)
-#cmd.png(os.path.abspath(outfp))
+cmd.png(os.path.abspath(outfp))
+print('-----Finished drawing from top -----')
+
+# Set the view from the bottom
+cmd.set_view (\
+    '-0.062551118,    0.668488383,   -0.741087615,\
+     0.326947540,   -0.687838376,   -0.648059845,\
+    -0.942967236,   -0.282835305,   -0.175534144,\
+     0.000000000,    0.000000000, -294.413665771,\
+   -13.488616943,  -48.363307953,  -22.056068420,\
+   232.117980957,  356.709350586,  -20.000000000' )
+#cmd.ray()
+outfp = os.path.join(DATA_DIR,os.path.splitext(edgef)[0]+'_hex_bottom.png')
+print('*'*5+'Image:%s'%os.path.abspath(outfp)+'*'*5)
+cmd.png(os.path.abspath(outfp))
+
+print('-----Finished drawing from bottom -----')
+#
+##---------------------------------------------------------------
+## Draw edges across
+
+# Delete the previous edges
+cmd.delete('dist*')
+
+for n,e in  enumerate(edges) : # For each edge
+    for ch1,ch2 in  combinations(chains,2) : # For each chain
+       for ii in range(2) : # Draw edges in both directions
+           if ii %2 == 1:
+               ch1,ch2 = ch2,ch1
+       	   t = cmd.dist('dist'+ch1+ch2+str(n),'3H4E//'+ch1+'/'+e[0]+'/CA',\
+       		'3H4E//'+ch2+'/'+e[1]+'/CA')
+           #print('dist'+ch1+ch2+str(n))       		
+       	   if t < cutoff : 
+               cmd.delete('dist'+ch1+ch2+str(n))
+           else : 
+               cmd.color('blue','dist'+ch1+ch2+str(n))
+               cmd.hide('labels','dist'+ch1+ch2+str(n))
+               cmd.show('spheres','3H4E//'+ch1+'/'+e[0]+'/CA')
+               cmd.show('spheres','3H4E//'+ch2+'/'+e[1]+'/CA')
+               cmd.color('yellow','3H4E//'+ch1+'/'+e[0]+'/CA')
+               cmd.color('yellow','3H4E//'+ch2+'/'+e[1]+'/CA')
+    #print e
+    
+      
+#cmd.ray()
+outfp = os.path.join(DATA_DIR,os.path.splitext(edgef)[0]+'_hex_across.png')
+print('*'*5+'Image:%s'%os.path.abspath(outfp)+'*'*5)
+cmd.png(os.path.abspath(outfp))
+print('-----Finished drawing from top -----')
+
 
 print '-------Completed Call--------';
 
