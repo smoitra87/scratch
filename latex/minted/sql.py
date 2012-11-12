f5ddb12cb1580d7f305c21592e2312b6059df21f
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-11-12 12:07:03 -0500

    Finished scratching mint and it is time for lunch

---
+++ latex/minted/sql.py
@@ -0,0 +1,34 @@
+""" This script performs commits, rollbacks on error and executes 
+SQL statements as a string"""
+
+import sqlite3 as lite
+import sys
+
+
+try : 
+	con = lite.connect("test.db")
+	cur = con.cursor()
+	
+	cur.executescript("""
+		DROP TABLE IF EXISTS Cars;
+        CREATE TABLE Cars(Id INT, Name TEXT, Price INT);
+        INSERT INTO Cars VALUES(1,'Audi',52642);
+        INSERT INTO Cars VALUES(2,'Mercedes',57127);
+        INSERT INTO Cars VALUES(3,'Skoda',9000);
+        INSERT INTO Cars VALUES(4,'Volvo',29000);
+        INSERT INTO Cars VALUES(5,'Bentley',350000);
+        INSERT INTO Cars VALUES(6,'Citroen',21000);
+        INSERT INTO Cars VALUES(7,'Hummer',41400);
+        INSERT INTO Cars VALUES(8,'Volkswagen',21600);
+	""")
+	
+	con.commit()
+
+except lite.Error, e : 
+	con.rollback()
+	print "The Error is ", e.args[0]
+	sys.exit(1)
+
+finally : 
+	if con : 
+		con.close()
