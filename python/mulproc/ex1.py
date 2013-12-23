2b34e49238dedbab01c6d2b1e33bdf2ae1225d71
Subhodeep Moitra <smoitra@cs.cmu.edu>
2013-12-22 19:20:02 -0500

    Reformatted multiprocessing code

--- python/mulproc/ex1.py
+++ python/mulproc/ex1.py
@@ -1,16 +1,16 @@
 """ A program that executes parallel system calls """
 
-import multiprocessing, sys, os
+import multiprocessing
+import sys
+import os
 
 
-def worker() : 
-	os.system("~/runJalview")
+def worker():
+    os.system("~/runJalview")
 
-if __name__ == '__main__' :
-	jobs = []
-	for i in range(5) : 
-		p = multiprocessing.Process(target=worker);
-		jobs.append(p)
-		p.start()
-
-
+if __name__ == '__main__':
+    jobs = []
+    for i in range(5):
+        p = multiprocessing.Process(target=worker)
+        jobs.append(p)
+        p.start()
--- python/mulproc/ex3.py
+++ python/mulproc/ex3.py
@@ -1,35 +1,38 @@
 """ Following the example of http://stackoverflow.com/questions/8533318/python-multiprocessing-pool-when-to-use-apply-apply-async-or-ma
 trying to write my own prog"""
 
-import os,sys
+import os
+import sys
 from multiprocessing import Pool
 from random import randint
 import time
 
-def foo(x) :
-	wt = randint(1,5) 
-	time.sleep(wt)
-	print("x={};wt={};ct={}".format(x,wt,time.ctime()))
-	return x
+
+def foo(x):
+    wt = randint(1, 5)
+    time.sleep(wt)
+    print("x={};wt={};ct={}".format(x, wt, time.ctime()))
+    return x
 
 results_async = []
-def log_results(result) :
-	results_async.append(result)
 
 
+def log_results(result):
+    results_async.append(result)
 
-if  __name__ == '__main__' : 
-	pool = Pool(processes=10)
-	print("Program started at {}".format(time.ctime()))
-	print("Starting Apply processes")
-	results = pool.map(foo,range(10))
-	print(results)
-	print("Starting apply_async processes")
-	results = pool.map_async(foo,range(10),callback=log_results)
-	
-	print("results_async at time{}:{}".format(time.ctime(),results_async))
-	time.sleep(3)	
-	print("results_async at time{}:{}".format(time.ctime(),results_async))
-	pool.close()
-	pool.join()
-	print("results_async at time{}:{}".format(time.ctime(),results_async))
+
+if __name__ == '__main__':
+    pool = Pool(processes=10)
+    print("Program started at {}".format(time.ctime()))
+    print("Starting Apply processes")
+    results = pool.map(foo, range(10))
+    print(results)
+    print("Starting apply_async processes")
+    results = pool.map_async(foo, range(10), callback=log_results)
+
+    print("results_async at time{}:{}".format(time.ctime(), results_async))
+    time.sleep(3)
+    print("results_async at time{}:{}".format(time.ctime(), results_async))
+    pool.close()
+    pool.join()
+    print("results_async at time{}:{}".format(time.ctime(), results_async))
