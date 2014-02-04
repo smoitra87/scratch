e86d0cc77316455d7ed7e395729e52c1a7a4d2cd
Subhodeep Moitra <smoitra@cs.cmu.edu>
2014-02-04 17:08:02 -0500

    Using Selenium for web browser automation

---
+++ python/selenium/ex2.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
+import time
+
+
+# Create a new instance of the Firefox driver
+driver = webdriver.Firefox()
+
+# go to the google home page
+driver.get("http://finance.yahoo.com/stock-center/")
+
+# the page is ajaxy so the title is originally this:
+print driver.title
+
+gainersElement = driver.find_element_by_link_text("% Gainers")
+1/0
+
+time.sleep(10)
+
+driver.quit()
+
+#
+## find the element that's name attribute is q (the google search box)
+#inputElement = driver.find_element_by_name("q")
+#
+## type in the search
+#inputElement.send_keys("cheese!")
+#
+## submit the form (although google automatically searches now without
+## submitting)
+#inputElement.submit()
+#
+#try:
+#    # we have to wait for the page to refresh, the last thing that seems to
+#    # be updated is the title
+#    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
+#    # You should see "cheese! - Google Search"
+#    print driver.title
+#
+#finally:
+#        driver.quit()
