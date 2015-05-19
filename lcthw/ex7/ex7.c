9dbec6847c3f829ee6d2b7c4036875fe3332cc5c
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2015-05-19 15:22:16 -0400

    ginishes ex7

---
+++ lcthw/ex7/ex7.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	int bugs = 100;
+	double bug_rate = 1.2;
+
+	printf("You have %d bugs at the imaginary rate of %f.\n", 
+			bugs, bug_rate);
+
+	long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	universe_of_defects = universe_of_defects * 1024L * 1024L * 1024L;
+	printf("The entire universe has %ld bugs.\n",
+		universe_of_defects);
+
+	double expected_bugs = bugs * bug_rate;
+	printf("You are expectec to have %f bugs/\n", expected_bugs);
+
+	double part_of_universe = expected_bugs / universe_of_defects;
+	printf("This is only a %e portion of the universe.\n", part_of_universe);
+
+	// This makes no sense. just a demo of something weird
+	char nul_byte = '\0';
+	int care_percentage = bugs * nul_byte;
+	printf("Which means you should care %d%%. \n", care_percentage)	;
+
+	// Trying to print out the null byte variable %s vs %c
+	printf("Print null byte with %%c %c.\n", nul_byte);
+	printf("Print null byte with %%s %s.\n", nul_byte);
+
+	return 0;
+}
+
